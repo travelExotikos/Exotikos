e2468771081020951b8193d64d71ead15b178d6b
Adriana Jaźbor-Indyk <adajazbor@gmail.com>
2016-11-12 11:19:54 -0800

    added intent

--- app/src/main/java/com/exotikosteam/exotikos/activities/MainActivity.java
+++ app/src/main/java/com/exotikosteam/exotikos/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.exotikosteam.exotikos.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -13,15 +15,27 @@ import com.exotikosteam.exotikos.fragments.TravelScanFragment;
 import com.exotikosteam.exotikos.fragments.TravelSummaryFragment;
 import com.exotikosteam.exotikos.models.trip.FlightStep;
 import com.exotikosteam.exotikos.models.trip.TripStatus;
+import com.exotikosteam.exotikos.utils.Constants;
 import com.exotikosteam.exotikos.webservices.flightstats.AirlinesApiEndpoint;
 import com.exotikosteam.exotikos.webservices.flightstats.AirportsApiEndpoint;
 import com.exotikosteam.exotikos.webservices.flightstats.FlightStatusApiEndpoint;
 import com.exotikosteam.exotikos.webservices.flightstats.SchedulesApiEndpoint;
+import com.microblink.activity.Pdf417ScanActivity;
+import com.microblink.recognizers.BaseRecognitionResult;
+import com.microblink.recognizers.RecognitionResults;
+import com.microblink.recognizers.blinkbarcode.BarcodeType;
+import com.microblink.recognizers.blinkbarcode.bardecoder.BarDecoderScanResult;
+import com.microblink.recognizers.blinkbarcode.pdf417.Pdf417ScanResult;
+import com.microblink.recognizers.blinkbarcode.zxing.ZXingScanResult;
+import com.microblink.results.barcode.BarcodeDetailedData;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements TravelScanFragment.OnScanCompletedListener,
-OnButtonsClicks{
+OnButtonsClicks {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private TripStatus trip;
@@ -167,5 +181,129 @@ OnButtonsClicks{
         if(buttonName.equals("LaunchScanPage")) {
             showTravelScanFragment();
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.REQUEST_CODE_SCAN) {
+            if (resultCode != Pdf417ScanActivity.RESULT_OK) {
+                Log.e(TAG, "Scan error " + resultCode);
+                return;
+            }
+            // First, obtain recognition result
+            RecognitionResults results = data.getParcelableExtra(Pdf417ScanActivity.EXTRAS_RECOGNITION_RESULTS);
+            // Get scan results array. If scan was successful, array will contain at least one element.
+            // Multiple element may be in array if multiple scan results from single image were allowed in settings.
+            BaseRecognitionResult[] resultArray = results.getRecognitionResults();
+
+            // Each recognition result corresponds to active recognizer. As stated earlier, there are 3 types of
+            // recognizers available (PDF417, Bardecoder and ZXing), so there are 3 types of results
+            // available.
+
+            StringBuilder sb = new StringBuilder();
+
+            for(BaseRecognitionResult res : resultArray) {
+                if(res instanceof Pdf417ScanResult) { // check if scan result is result of Pdf417 recognizer
+                    Pdf417ScanResult result = (Pdf417ScanResult) res;
+                    // getStringData getter will return the string version of barcode contents
+                    String barcodeData = result.getStringData();
+                    // isUncertain getter will tell you if scanned barcode contains some uncertainties
+                    boolean uncertainData = result.isUncertain();
+                    // getRawData getter will return the raw data information object of barcode contents
+                    BarcodeDetailedData rawData = result.getRawData();
+                    // BarcodeDetailedData contains information about barcode's binary layout, if you
+                    // are only interested in raw bytes, you can obtain them with getAllData getter
+                    byte[] rawDataBuffer = rawData.getAllData();
+
+                    // if data is URL, open the browser and stop processing result
+                    if(checkIfDataIsUrlAndCreateIntent(barcodeData)) {
+                        return;
+                    } else {
+                        // add data to string builder
+                        sb.append("PDF417 scan data");
+                        if (uncertainData) {
+                            sb.append("This scan data is uncertain!\n\n");
+                        }
+                        sb.append(" string data:\n");
+                        sb.append(barcodeData);
+                        if (rawData != null) {
+                            sb.append("\n\n");
+                            sb.append("PDF417 raw data:\n");
+                            sb.append(rawData.toString());
+                            sb.append("\n");
+                            sb.append("PDF417 raw data merged:\n");
+                            sb.append("{");
+                            for (int i = 0; i < rawDataBuffer.length; ++i) {
+                                sb.append((int) rawDataBuffer[i] & 0x0FF);
+                                if (i != rawDataBuffer.length - 1) {
+                                    sb.append(", ");
+                                }
+                            }
+                            sb.append("}\n\n\n");
+                        }
+                    }
+                } else if(res instanceof BarDecoderScanResult) { // check if scan result is result of BarDecoder recognizer
+                    BarDecoderScanResult result = (BarDecoderScanResult) res;
+                    // with getBarcodeType you can obtain barcode type enum that tells you the type of decoded barcode
+                    BarcodeType type = result.getBarcodeType();
+                    // as with PDF417, getStringData will return the string contents of barcode
+                    String barcodeData = result.getStringData();
+                    if(checkIfDataIsUrlAndCreateIntent(barcodeData)) {
+                        return;
+                    } else {
+                        sb.append(type.name());
+                        sb.append(" string data:\n");
+                        sb.append(barcodeData);
+                        sb.append("\n\n\n");
+                    }
+                } else if(res instanceof ZXingScanResult) { // check if scan result is result of ZXing recognizer
+                    ZXingScanResult result= (ZXingScanResult) res;
+                    // with getBarcodeType you can obtain barcode type enum that tells you the type of decoded barcode
+                    BarcodeType type = result.getBarcodeType();
+                    // as with PDF417, getStringData will return the string contents of barcode
+                    String barcodeData = result.getStringData();
+                    if(checkIfDataIsUrlAndCreateIntent(barcodeData)) {
+                        return;
+                    } else {
+                        sb.append(type.name());
+                        sb.append(" string data:\n");
+                        sb.append(barcodeData);
+                        sb.append("\n\n\n");
+                    }
+                }
+            }
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, sb.toString());
+            startActivity(Intent.createChooser(intent, getString(R.string.UseWith)));
+        }
+    }
+
+    /**
+     * Checks whether data is URL and in case of URL data creates intent and starts activity.
+     * @param data String to check.
+     * @return If data is URL returns {@code true}, else returns {@code false}.
+     */
+    // nice featute but we do not need it
+    private boolean checkIfDataIsUrlAndCreateIntent(String data) {
+        // if barcode contains URL, create intent for browser
+        // else, contain intent for message
+        boolean barcodeDataIsUrl;
+        try {
+            @SuppressWarnings("unused")
+            URL url = new URL(data);
+            barcodeDataIsUrl = true;
+        } catch (MalformedURLException exc) {
+            barcodeDataIsUrl = false;
+        }
+
+        if (barcodeDataIsUrl) {
+            // create intent for browser
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(data));
+            startActivity(Intent.createChooser(intent, getString(R.string.UseWith)));
+        }
+        return barcodeDataIsUrl;
     }
 }
--- app/src/main/java/com/exotikosteam/exotikos/fragments/TravelScanFragment.java
+++ app/src/main/java/com/exotikosteam/exotikos/fragments/TravelScanFragment.java
@@ -1,6 +1,7 @@
 package com.exotikosteam.exotikos.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,11 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.exotikosteam.exotikos.R;
 import com.exotikosteam.exotikos.databinding.TravelScanFragmentBinding;
 import com.exotikosteam.exotikos.models.trip.TripStatus;
+import com.exotikosteam.exotikos.utils.Constants;
+import com.microblink.activity.Pdf417ScanActivity;
+import com.microblink.recognizers.blinkbarcode.bardecoder.BarDecoderRecognizerSettings;
+import com.microblink.recognizers.blinkbarcode.pdf417.Pdf417RecognizerSettings;
+import com.microblink.recognizers.blinkbarcode.zxing.ZXingRecognizerSettings;
+import com.microblink.recognizers.settings.RecognitionSettings;
+import com.microblink.recognizers.settings.RecognizerSettings;
+import com.microblink.util.Log;
 
 /**
  * Created by lramaswamy on 11/11/16.
@@ -23,6 +31,7 @@ public class TravelScanFragment extends Fragment {
 
     private OnScanCompletedListener listener;
     private TravelScanFragmentBinding scanFragmentBinding;
+    private static String TAG = "ScanAction";
 
     ImageView scanImageClick;
 
@@ -57,15 +66,112 @@ public class TravelScanFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         scanImageClick.setOnClickListener(v -> {
-            launchCameraView();
+            launchCameraView(v);
         });
     }
 
-    private void launchCameraView() {
-        Toast.makeText(getContext(), "Camera View Launched", Toast.LENGTH_SHORT).show();
-        //@TODO once the boarding pass is scanned, the data needs to be generated and passed to the activity
-        TripStatus trip = TripStatus.newMockInstance();
-        scanCompleted(trip);
+    private void launchCameraView(View view) {
+        Log.i(TAG, "scan will be performed");
+        // Intent for ScanActivity
+        Intent intent = new Intent(view.getContext(), Pdf417ScanActivity.class);
+
+        // If you want sound to be played after the scanning process ends,
+        // put here the resource ID of your sound file. (optional)
+       // intent.putExtra(Pdf417ScanActivity.EXTRAS_BEEP_RESOURCE, R.raw.beep);
+
+        // In order for scanning to work, you must enter a valid licence key. Without licence key,
+        // scanning will not work. Licence key is bound the the package name of your app, so when
+        // obtaining your licence key from Microblink make sure you give us the correct package name
+        // of your app. You can obtain your licence key at http://microblink.com/login or contact us
+        // at http://help.microblink.com.
+        // Licence key also defines which recognizers are enabled and which are not. Since the licence
+        // key validation is performed on image processing thread in native code, all enabled recognizers
+        // that are disallowed by licence key will be turned off without any error and information
+        // about turning them off will be logged to ADB logcat.
+        intent.putExtra(Pdf417ScanActivity.EXTRAS_LICENSE_KEY, Constants.PDF417_KEY);
+        // If you want to open front facing camera, uncomment the following line.
+        // Note that front facing cameras do not have autofocus support, so it will not
+        // be possible to scan denser and smaller codes.
+//        intent.putExtra(Pdf417ScanActivity.EXTRAS_CAMERA_TYPE, (Parcelable) CameraType.CAMERA_FRONTFACE);
+
+        // You need to define array of recognizer settings. There are 4 types of recognizers available
+        // in PDF417.mobi SDK.
+        // Don't enable recognizers and barcode types which you don't actually use because this will
+        // significantly decrease the scanning speed.
+
+        // Pdf417RecognizerSettings define the settings for scanning plain PDF417 barcodes.
+        Pdf417RecognizerSettings pdf417RecognizerSettings = new Pdf417RecognizerSettings();
+        // Set this to true to scan barcodes which don't have quiet zone (white area) around it
+        // Use only if necessary because it drastically slows down the recognition process
+        pdf417RecognizerSettings.setNullQuietZoneAllowed(true);
+        // Set this to true to scan even barcode not compliant with standards
+        // For example, malformed PDF417 barcodes which were incorrectly encoded
+        // Use only if necessary because it slows down the recognition process
+//        pdf417RecognizerSettings.setUncertainScanning(true);
+
+        // BarDecoderRecognizerSettings define settings for scanning 1D barcodes with algorithms
+        // implemented by Microblink team.
+        BarDecoderRecognizerSettings oneDimensionalRecognizerSettings = new BarDecoderRecognizerSettings();
+        // set this to true to enable scanning of Code 39 1D barcodes
+        oneDimensionalRecognizerSettings.setScanCode39(true);
+        // set this to true to enable scanning of Code 128 1D barcodes
+        oneDimensionalRecognizerSettings.setScanCode128(true);
+        // set this to true to use heavier algorithms for scanning 1D barcodes
+        // those algorithms are slower, but can scan lower resolution barcodes
+//        oneDimensionalRecognizerSettings.setTryHarder(true);
+
+        // ZXingRecognizerSettings define settings for scanning barcodes with ZXing library
+        // We use modified version of ZXing library to support scanning of barcodes for which
+        // we still haven't implemented our own algorithms.
+        ZXingRecognizerSettings zXingRecognizerSettings = new ZXingRecognizerSettings();
+        // set this to true to enable scanning of QR codes
+        zXingRecognizerSettings.setScanQRCode(true);
+        zXingRecognizerSettings.setScanITFCode(true);
+
+        // finally, when you have defined settings for each recognizer you want to use,
+        // you should put them into array held by global settings object
+
+        RecognitionSettings recognitionSettings = new RecognitionSettings();
+        // add settings objects to recognizer settings array
+        // Pdf417Recognizer, BarDecoderRecognizer and ZXingRecognizer
+        //  will be used in the recognition process
+        recognitionSettings.setRecognizerSettingsArray(new RecognizerSettings[]{pdf417RecognizerSettings, oneDimensionalRecognizerSettings, zXingRecognizerSettings});
+
+        // additionally, there are generic settings that are used by all recognizers or the
+        // whole recognition process
+
+        // by default, this option is true, which means that it is possible to obtain multiple
+        // recognition results from the same image.
+        // if you want to obtain one result from the first successful recognizer
+        // (when first barcode is found, no matter which type) set this option to false
+//        recognitionSettings.setAllowMultipleScanResultsOnSingleImage(false);
+
+        // finally send that settings object over intent to scan activity
+        // use Pdf417ScanActivity.EXTRAS_RECOGNITION_SETTINGS to set recognizer settings
+        intent.putExtra(Pdf417ScanActivity.EXTRAS_RECOGNITION_SETTINGS, recognitionSettings);
+
+        // if you do not want the dialog to be shown when scanning completes, add following extra
+        // to intent
+//        intent.putExtra(Pdf417ScanActivity.EXTRAS_SHOW_DIALOG_AFTER_SCAN, false);
+
+        // if you want to enable pinch to zoom gesture, add following extra to intent
+        intent.putExtra(Pdf417ScanActivity.EXTRAS_ALLOW_PINCH_TO_ZOOM, true);
+
+        // if you want Pdf417ScanActivity to display rectangle where camera is focusing,
+        // add following extra to intent
+        intent.putExtra(Pdf417ScanActivity.EXTRAS_SHOW_FOCUS_RECTANGLE, true);
+
+        // if you want to use camera fit aspect mode to letterbox the camera preview inside
+        // available activity space instead of cropping camera frame (default), add following
+        // extra to intent.
+        // Camera Fit mode does not look as nice as Camera Fill mode on all devices, especially on
+        // devices that have very different aspect ratios of screens and cameras. However, it allows
+        // all camera frame pixels to be processed - this is useful when reading very large barcodes.
+//        intent.putExtra(Pdf417ScanActivity.EXTRAS_CAMERA_ASPECT_MODE, (Parcelable) CameraAspectMode.ASPECT_FIT);
+
+        // Start Activity
+        startActivityForResult(intent, Constants.REQUEST_CODE_SCAN);
+
     }
 
     public static TravelScanFragment newInstance(String title) {
