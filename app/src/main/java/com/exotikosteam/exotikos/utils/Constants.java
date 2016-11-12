729379e34e21f13109c4086f9d9513b89b0ab0fe
Adriana Jaźbor-Indyk <adajazbor@gmail.com>
2016-11-12 09:57:57 -0800

    add pdf417 libraries

---
+++ app/src/main/java/com/exotikosteam/exotikos/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.exotikosteam.exotikos.utils;
+
+/**
+ * Created by ada on 11/12/16.
+ */
+
+public class Constants {
+
+    public static final String PDF417_KEY = "2YPPHKVV-SSO7YBVP-BY5GGPXK-XHGUJ7UM-RDZPXH3V-72GIR4X3-T5275DEI-6L54556L";
+}
---
+++ app/src/main/java/com/exotikosteam/exotikos/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.exotikosteam.exotikos.utils;
+
+/**
+ * Created by ada on 11/12/16.
+ */
+
+public class Utils {
+}
