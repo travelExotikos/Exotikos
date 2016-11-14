180d7fade15f17ffbbf376024e7e51f5a4713e66
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-11-13 20:43:51 -0800

    airline picker dialog

--- app/src/main/java/com/exotikosteam/exotikos/activities/NewTripActivity.java
+++ app/src/main/java/com/exotikosteam/exotikos/activities/NewTripActivity.java
@@ -1,15 +1,38 @@
 package com.exotikosteam.exotikos.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.exotikosteam.exotikos.R;
+import com.exotikosteam.exotikos.fragments.AirlinePickDialogFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewTripActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSelectAirline) Button btnSelectAirline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
+        ButterKnife.bind(this);
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        btnSelectAirline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                AirlinePickDialogFragment pickAirlineDialogFragment = AirlinePickDialogFragment.newInstance();
+                pickAirlineDialogFragment.show(fm, "fragment_dialog_pick_airline");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/exotikosteam/exotikos/fragments/AirlinePickDialogFragment.java
@@ -0,0 +1,121 @@
+package com.exotikosteam.exotikos.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.exotikosteam.exotikos.ExotikosApplication;
+import com.exotikosteam.exotikos.R;
+import com.exotikosteam.exotikos.adapters.AirlinesAdapter;
+import com.exotikosteam.exotikos.models.airline.Airline;
+import com.exotikosteam.exotikos.webservices.flightstats.AirlinesApiEndpoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
+
+/**
+ * Created by jesusft on 11/13/16.
+ */
+
+public class AirlinePickDialogFragment extends DialogFragment {
+
+    @BindView(R.id.etName) EditText etName;
+    @BindView(R.id.lvAirlines) ListView lvAirlines;
+
+    private List<Airline> airlines;
+    private AirlinesAdapter adapter;
+    private final PublishSubject<Airline> selectSubject = PublishSubject.create();
+    private AirlinesApiEndpoint airlinesService;
+
+    public PublishSubject<Airline> getSelectSubject() {
+        return selectSubject;
+    }
+
+    public static AirlinePickDialogFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        AirlinePickDialogFragment fragment = new AirlinePickDialogFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getDialog().setTitle(R.string.pick_airline);
+
+        final View v = inflater.inflate(R.layout.fragment_airline_pick, container);
+        ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        airlines = new ArrayList<>();
+        ExotikosApplication app = ((ExotikosApplication)getContext().getApplicationContext());
+        airlinesService = app.getAirlinesService();
+        airlinesService.getActive(app.getFligthStatsAppID(), app.getFligthStatsAppKey())
+                .concatMapIterable(airlinesResponse -> airlinesResponse.getAirlines())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(airline -> {
+                    airlines.add(airline);
+                    adapter.notifyDataSetChanged();
+                });
+
+        setupList();
+        setupListeners();
+    }
+
+    private void setupList() {
+        adapter = new AirlinesAdapter(getContext(), airlines);
+        lvAirlines.setAdapter(adapter);
+    }
+
+    private void setupListeners() {
+        lvAirlines.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectSubject.onNext(airlines.get(i));
+                selectSubject.onCompleted();
+            }
+        });
+
+        etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 0 || charSequence.length() >= 2) {
+                    adapter.getFilter().filter(charSequence.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
