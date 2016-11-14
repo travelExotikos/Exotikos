67562b6e65c75c6a6fd30ef4b6459193c510c1bb
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-11-13 22:07:16 -0800

    some refactoring

--- app/src/main/java/com/exotikosteam/exotikos/activities/NewTripActivity.java
+++ app/src/main/java/com/exotikosteam/exotikos/activities/NewTripActivity.java
@@ -1,14 +1,18 @@
 package com.exotikosteam.exotikos.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.exotikosteam.exotikos.R;
 import com.exotikosteam.exotikos.fragments.AirlinePickDialogFragment;
 import com.exotikosteam.exotikos.models.airline.Airline;
+
+import org.parceler.Parcels;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +22,9 @@ public class NewTripActivity extends AppCompatActivity {
     private Airline mSelectedAirline;
 
     @BindView(R.id.btnSelectAirline) Button btnSelectAirline;
+    @BindView(R.id.btnSelectFlights) Button btnSelectFlights;
+    @BindView(R.id.etFlightNumber) EditText etFlightNumber;
+    @BindView(R.id.dpDepartureDate) DatePicker dpDepartureDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +32,35 @@ public class NewTripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_trip);
         ButterKnife.bind(this);
 
+        getSupportActionBar().setTitle(R.string.new_trip);
+
         setupListeners();
+
+        // TODO ticket scan
     }
 
     private void setupListeners() {
-        btnSelectAirline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fm = getSupportFragmentManager();
-                AirlinePickDialogFragment pickAirlineDialogFragment = AirlinePickDialogFragment.newInstance();
-                pickAirlineDialogFragment.getSelectSubject()
-                        .subscribe(airline -> {
-                            mSelectedAirline = airline;
-                            btnSelectAirline.setText(airline.getName());
-                            pickAirlineDialogFragment.dismiss();
-                        });
-                pickAirlineDialogFragment.show(fm, "fragment_dialog_pick_airline");
+        btnSelectAirline.setOnClickListener(view -> {
+            FragmentManager fm = getSupportFragmentManager();
+            AirlinePickDialogFragment pickAirlineDialogFragment = AirlinePickDialogFragment.newInstance();
+            pickAirlineDialogFragment.getSelectSubject()
+                    .subscribe(airline -> {
+                        mSelectedAirline = airline;
+                        btnSelectAirline.setText(airline.getName());
+                        pickAirlineDialogFragment.dismiss();
+                    });
+            pickAirlineDialogFragment.show(fm, "fragment_dialog_pick_airline");
+        });
 
-
-            }
+        btnSelectAirline.setOnClickListener(view -> {
+            // TODO where to go?
+            Intent i = new Intent(NewTripActivity.this, null);
+            i.putExtra("airline", Parcels.wrap(mSelectedAirline));
+            i.putExtra("year", dpDepartureDate.getYear());
+            i.putExtra("month", dpDepartureDate.getMonth());
+            i.putExtra("day", dpDepartureDate.getDayOfMonth());
+            i.putExtra("flighNumber", Integer.valueOf(etFlightNumber.getText().toString()));
+            startActivity(i);
         });
     }
 }
